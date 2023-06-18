0f3220907322936e04f573bfd80603c797625e8e
i050 <59620500+i050@users.noreply.github.com>
2023-06-18 11:03:43 +0300

    Update Program.cs
    mashu acher

--- issacHershkop2/Program.cs
+++ issacHershkop2/Program.cs
@@ -10,7 +10,7 @@ namespace issacHershkop2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( "hello world" );
+            Console.WriteLine( "mashu acher !!!!!!!!!!!!!!!!!" );
         }
     }
 }
